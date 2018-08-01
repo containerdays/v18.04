dca2173ac1c00297fa2bc23b19abab923d3af132
Noriyuki Suzuki <suzuki-n@impress.co.jp>
2018-08-01 19:32:59 +0900

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2018/06/21',function(event){
+     jQuery('#clock').countdown('2018/12/03',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
